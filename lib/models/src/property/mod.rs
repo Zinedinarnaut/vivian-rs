f16ec484bc5b8d1188f67d7ebbb3f0c968667135
ZinedinArnaut <zinedinarnaut085@gmail.com>
2025-05-29 09:07:53 +1000

    Update

--- lib/models/src/property/mod.rs
+++ lib/models/src/property/mod.rs
@@ -3,14 +3,12 @@ mod math_ext;
 mod primitive;
 mod random;
 mod set;
-mod random;
 
 pub use map::PropertyHashMap;
 pub use math_ext::PropertyTransform;
 pub use primitive::PrimitiveProperty;
 pub use random::GachaRandom;
 pub use set::PropertyHashSet;
-pub use random::GachaRandom;
 
 pub trait Property {
     fn is_changed(&self) -> bool;
